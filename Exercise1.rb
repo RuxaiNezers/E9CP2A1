327d6f7083d48ac8413e058fa93c009a84a114c3
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-28 20:36:30 -0400

    exercise 1 solved

---
+++ Exercise1.rb
@@ -0,0 +1,36 @@
+class Table
+
+  attr_accessor :table_name, :recaudations
+  def initialize(name, *recaudations)
+  	@table_name = name
+  	@recaudations = recaudations.map(&:to_i)
+  end
+
+  def self.build(file)
+  	tables = []
+  	File.readlines(file).each do |line|
+  		table_info_array = line.chomp.split(', ')
+  		name, recaudations = table_info_array.shift, table_info_array
+  		tables << Table.new(name, *recaudations)
+  	end
+  	tables
+  end
+
+  def self.bigger(file)
+  	table = build(file)
+  	table.each {|ele| puts "#{ele.table_name} >> #{ele.recaudations.max} day #{ele.recaudations.index(ele.recaudations.max)+1}"}
+  end
+
+  def self.average(file)
+  	table = build(file)
+  	table.each do |ele|
+  	average = ele.recaudations.inject(&:+) /ele.recaudations.size.to_f
+  		puts "#{ele.table_name}: #{average}"
+ 	end 
+
+
+  end
+end
+
+Table.bigger('casino.txt')
+Table.average('casino.txt')
